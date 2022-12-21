25c6f0047117fc763a26d42add211852823ce7dc
Carlos Leonardo <litoyork@gmail.com>
2022-12-21 14:49:45 -0500

    semana3

--- Ejemplosc3/src/paquete01/Ejemplo021.java
+++ Ejemplosc3/src/paquete01/Ejemplo021.java
@@ -5,18 +5,20 @@
  */
 package paquete01;
 
+import java.util.Locale;
+
 /**
  *
  * @author reroes
  */
-public class Ejemplo02 {
+public class Ejemplo021 {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // Creación de arreglo bidimensionales
-        
+        String cadenaFinal = "";
         // forma 1
         int[][] arreglo1 = new int[3][4];
         System.out.printf("Uso de la opción length: %d\n", arreglo1.length);
@@ -28,11 +30,13 @@ public class Ejemplo02 {
                
         for (int fila = 0; fila < arreglo1.length; fila++) {
             for (int col = 0; col < arreglo1[fila].length; col++) {
-                System.out.printf("fila[%d] columna[%d] = %d\n", 
-                        fila, col, arreglo1[fila][col]);
+                //System.out.printf("fila[%d] columna[%d] = %d\n", 
+                //        fila, col, arreglo1[fila][col]);
+                cadenaFinal = String.format("%sfila[%d] columna[%d] = %d/n",
+                           cadenaFinal,fila, col, arreglo1[fila][col]);          
             }
         }
-        
+        System.out.printf("%s\n", cadenaFinal);
     }
     
 }
